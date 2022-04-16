afa0ba0838d42f69d8991cd8f41e324aea48d101
kaniraerlna <73452737+kaniraerlna@users.noreply.github.com>
2022-04-16 12:58:04 +0700

    Add files via upload

---
+++ Studio3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author Miss KaNiRa
+ */
+public class Studio3 extends Studio{
+    
+    public String studio3(){
+        return "3";
+    }
+}
---
+++ Studio5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author Miss KaNiRa
+ */
+public class Studio5 extends Studio{
+    
+    public String studio5(){
+        return "5";
+    }
+}
